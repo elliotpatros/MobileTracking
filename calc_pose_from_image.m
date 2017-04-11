20fe52d7f2187f2c44900b6c5a54b24716289e23
Elliot Patros <empatros@gmail.com>
2017-04-11 14:04:23 -0700

    added new resources and a static pose estimation script

--- calc_pose_from_image.m
+++ calc_pose_from_image.m
@@ -38,38 +38,30 @@ X = X(1:3,1:3);
 sx = [worldPoints(1,1), worldPoints(end,1), worldPoints(1,1), worldPoints(end,1)];
 sy = [worldPoints(1,2), worldPoints(1,2), worldPoints(end,2), worldPoints(end,2)];
 sz = zeros(4);
-    
-%% set up stream
-url = ['http://', ip_addr, ':8080/shot.jpg'];
-is_streaming = true;
 
-%% stream
-disp('streaming...');
-while(is_streaming)
-    %% get current frame from video stream
-    try
-        frame = rgb2gray(imread(url));
-    catch
-        is_streaming = false;
-        disp('stream closed');
-        break;
-    end
-    
-    %% plot world points
-    surf(sx, sy, sz);
-    hold on;
-    plot3(0, 0, 0, 'ro');
+%% get current frame from video stream
+try
+    frame = rgb2gray(imread('./Resources/vsm_checkerboard-1.jpg'));
+catch
+    disp('stream closed');
+end
 
-    %% process image
-    % find camera extrinsics
-    frame = undistortImage(frame, cameraParameters);
-    imagePoints = detectCheckerboardPoints(frame);
-    
-    % if we didn't find a checkerboard, continue
-    if (length(imagePoints) ~= nImagePoints)
-        continue;
-    end
-    
+subplot(131);
+imshow(frame);
+
+%% plot world points
+subplot(1,3,[2,3]);
+surf(sx, sy, sz);
+hold on;
+plot3(0, 0, 0, 'ro');
+
+%% process image
+% find camera extrinsics
+frame = undistortImage(frame, cameraParameters);
+imagePoints = detectCheckerboardPoints(frame);
+
+% if we didn't find a checkerboard, continue
+if (length(imagePoints) == nImagePoints)
     [rotation, translation] = extrinsics(imagePoints, worldPoints, cameraParameters);
 
     % find camera pose
@@ -77,6 +69,7 @@ while(is_streaming)
     location = -translation/rotation;
 
     % plot camera
+   
     plotCamera('Location', location, 'Orientation', orientation, 'Opacity', 0.25, 'AxesVisible', true, 'Size', 5);
 
     %% viewing preferences
@@ -85,7 +78,6 @@ while(is_streaming)
     axis vis3d
     axis([-120, 120, -120, 80, -300, 0]);
     view(-10, -80);
-%     set(gca, 'CameraUpVector', [0 0 -1]);
+
     hold off;
-    drawnow;
 end
